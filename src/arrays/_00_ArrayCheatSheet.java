3c1fff68c224ee9d723050b8d8ade3cbc58a1efc
student <student@ip-172-31-62-136.us-west-2.compute.internal>
2021-05-31 13:27:00 -0700

    array Practice

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,19 +3,27 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String Marvel[] = {
+			"Iron Man", 
+			"Pip the Troll",
+			"Howard the Duck",
+			"Adam Warlock",
+			"Drax the Destroyer"
+		};
 		//2. print the third element in the array
-		
+		System.out.println(Marvel[2]);
 		//3. set the third element to a different value
-		
+		Marvel[2] = "Dr. Strange";
 		//4. print the third element again
-		
+		System.out.println(Marvel[3]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < Marvel.length; i++) {
+			System.out.println(Marvel[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int [] num = new int [50];
 		//7. use a for loop to make every value of the integer array a random number
 		
 		//8. without printing the entire array, print only the smallest number in the array
